454af2e4a842c4d29ec68ba77bd26ac14e764921
ArnavT27 <137822090+ArnavT27@users.noreply.github.com>
2023-07-26 23:00:54 +0530

    Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -8,7 +8,7 @@ public class Factorial
         Scanner s=new Scanner(System.in);
         System.out.println("Enter a number:");
         int n=s.nextInt();
-        int j=1;
+        long j=1;
         for (int i=1;i<=n;i++)
         {
             j=i*j;
