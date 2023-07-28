3cfc63ce4490d15341e1478301c4cffbe9e590c8
ArnavT27 <137822090+ArnavT27@users.noreply.github.com>
2023-07-28 12:28:15 +0530

    displayDigit.java
    to display digits of a number

---
+++ displayDigit.java
@@ -0,0 +1,19 @@
+import java.lang.*;
+import java.util.*;
+
+public class displayDigit
+{
+    public static void main (String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int n=s.nextInt();
+        int r;
+        while(n>0)
+        {
+            r=n%10;
+            n=n/10;
+            System.out.println(r);
+        }
+    }
+}
