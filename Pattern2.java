b5ec4a23c24db30b5053c9ba1695b5d305eb8a7e
ArnavT27 <137822090+ArnavT27@users.noreply.github.com>
2023-07-29 16:04:47 +0530

    Pattern2.java

---
+++ Pattern2.java
@@ -0,0 +1,22 @@
+import java.lang.*;
+import java.util.*;
+
+public class Pattern2
+{
+    public static void main (String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int n=s.nextInt();
+        int count=1;
+        for (int i=1;i<=n;i++)
+        {
+            for (int j=1;j<=n;j++)
+            {
+                System.out.format(" %2d ",count);
+                count++;
+            }
+            System.out.println();
+        }
+    }
+}
