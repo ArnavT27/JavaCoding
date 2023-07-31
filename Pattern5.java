cc8d52d4a4a38bffe53e4555b68b3d58ac7fc681
ArnavT27 <137822090+ArnavT27@users.noreply.github.com>
2023-07-31 12:08:36 +0530

    Pattern5.java

---
+++ Pattern5.java
@@ -0,0 +1,23 @@
+import java.lang.*;
+import java.util.*;
+
+public class Pattern5
+{
+    public static void main (String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int n=s.nextInt();
+        for (int i=1;i<=n;i++)
+        {
+            for (int j=1;j<=n;j++)
+            {
+                if (i<=j)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
