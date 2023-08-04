1302c768b01941ec4f654ecbbf126bb3ec243458
ArnavT27 <137822090+ArnavT27@users.noreply.github.com>
2023-08-04 09:50:07 +0530

    Create ArrayPractice1.java
    Contains 4 programs
    1.finding second largest number 
    2.finding largest number in array
    3.finding a number in array
    4.finding the sum of elements in array

---
+++ ArrayPractice1.java
@@ -0,0 +1,108 @@
+import java.lang.*;
+import java.util.*;
+
+public class ArrayPractice1
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int n=s.nextInt();
+        int A[]=new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i=0;i<A.length;i++)
+        {
+            A[i]=s.nextInt();
+        }
+        int max1,max2;
+        max1=max2=A[0];
+        for (int i=0;i<A.length;i++)
+        {
+            if (A[i]>max1)
+            {
+                max2=max1;
+                max1=A[i];
+            }
+            else if (A[i]>max2)
+                max2=A[i];
+        }
+        System.out.println("Second maximum number is :"+max2);
+
+
+
+
+
+//        FINDING MAXIMUM ELEMENT
+
+
+//        Scanner s=new Scanner(System.in);
+//        System.out.println("Enter the size of the array:");
+//        int n=s.nextInt();
+//        int A[]=new int[n];
+//        System.out.println("Enter elements of the array");
+//        for (int i=0;i<A.length;i++)
+//        {
+//            A[i]=s.nextInt();
+//        }
+//        int max=A[0];
+//        for (int i=0;i<A.length;i++)
+//        {
+//            if (A[i]>max)
+//            {
+//                max=A[i];
+//            }
+//        }
+//        System.out.println("Greatest number is :"+max);
+
+
+
+
+
+//        SEARCHING AN ELEMENT PROGRAM
+
+//        Scanner s=new Scanner(System.in);
+//        System.out.println("Enter Size of the array:");
+//        int n=s.nextInt();
+//        int A[]=new int[n];
+//        System.out.println("Enter elements of the array");
+//        for (int i=0;i<A.length;i++)
+//        {
+//            A[i]=s.nextInt();
+//        }
+//        System.out.println("Enter the number you want to find :");
+//        int key=s.nextInt();
+//        for (int i=0;i<A.length;i++)
+//        {
+//            if (key==A[i])
+//            {
+//                System.out.println("NUMBER FOUND");
+//                System.out.println("Your entered number index is:"+i);
+//                System.exit(0);
+//            }
+//        }
+//        System.out.println("NUMBER NOT FOUND");
+//
+
+
+
+
+
+
+
+
+//      SUM OF ARRAY PROGRAM
+
+//        Scanner s=new Scanner(System.in);
+//        System.out.println("Please enter the size of array:");
+//        int n=s.nextInt();
+//        int sum=0;
+//        int A[]=new int[n];
+//        System.out.println("Enter the elements of the array:");
+//        for (int i=0;i<n;i++)
+//        {
+//            A[i]=s.nextInt();
+//            sum=sum+A[i];
+//        }
+//        System.out.println("Sum of elements of the array is:"+sum);
+    }
+}
