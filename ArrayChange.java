92ad7a1728451ba2e279b48b63008809a9ce097b
ArnavT27 <137822090+ArnavT27@users.noreply.github.com>
2023-08-27 15:17:09 +0530

    Update ArrayChange.java

--- ArrayChange.java
+++ ArrayChange.java
@@ -14,16 +14,21 @@ public class ArrayChange
     public static void main(String args[])
     {
         Scanner s=new Scanner(System.in);
-        System.out.println("Enter the size of array:");
+        System.out.println("Enter the size of the array:");
         int size=s.nextInt();
         int A[]=new int[size];
+        System.out.println("Enter the elements of the array: ");
         for (int i=0;i<A.length;i++)
         {
             A[i]=s.nextInt();
         }
-        System.out.println("Enter the index of element which you want to change:");
+        System.out.println("Enter the index of the number you want to change :");
         int index=s.nextInt();
-        System.out.println("Enter the number which you want to put in that place:");
+        if (index>(size-1))
+            System.out.println("Invalid index");
+        else {
+            System.out.println("Enter the number which you want to put in that place:");
+        }
         int number=s.nextInt();
         change(A,index,number);
     }
