ad9c4aa9fa53c5b4fbf5e578fd535ade8582b7a8
ArnavT27 <137822090+ArnavT27@users.noreply.github.com>
2023-10-16 19:59:10 +0530

    Create DescendingBubbleSort.java

---
+++ DescendingBubbleSort.java
@@ -0,0 +1,40 @@
+import java.lang.*;
+import java.util.*;
+
+public class DescendingBubbleSort
+{
+    static void sorted(int arr[])
+    {
+        for(int i=0;i<arr.length-1;i++)
+        {
+            for(int j=0;j<arr.length-i-1;j++)
+            {
+                if(arr[j]<arr[j+1])
+                {
+                    // descending swap using bubble sort
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        System.out.println("Sorted array is:");
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter the size of the array :");
+        int size=s.nextInt();
+        int arr[]=new int[size];
+        System.out.println("Enter the elements of the array:");
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i]=s.nextInt();
+        }
+        sorted(arr);
+    }
+}
