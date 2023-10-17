6f09b7c5a1949a60b0ba3898432274f23220c04f
ArnavT27 <137822090+ArnavT27@users.noreply.github.com>
2023-10-17 16:59:43 +0530

    Create SortingUsingSelectionSort.java

---
+++ SortingUsingSelectionSort.java
@@ -0,0 +1,43 @@
+import java.lang.*;
+import java.util.*;
+
+public class SortingUsingSelectionSort.java
+{
+    static void sorted(int arr[])
+    {
+        // time complexity = O(n^2)
+        for(int i=0;i<arr.length-1;i++)
+        {
+            int smallest=i;
+            for(int j=i+1;j<arr.length;j++)
+            {
+                if(arr[smallest]>arr[j])
+                {
+                  // ascending order sorting using selection sort
+                    smallest=j;
+                }
+            }
+            int temp=arr[smallest];
+            arr[smallest]=arr[i];
+            arr[i]=temp;
+        }
+        System.out.println("Sorted array is:");
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter the size of the array :");
+        int size=s.nextInt();
+        int arr[]=new int[size];
+        System.out.println("Enter the elements of the array:");
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i]=s.nextInt();
+        }
+        sorted(arr);
+    }
+}
